06cad432c67534b235b4aa2fdb583e3b39c7005e
Hunter Fehr <hunter.fehr3935@yahoo.com>
2022-12-13 16:10:40 -0800

    redone labe 11 to learn how to lay out everything

---
+++ vendor/index.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const eventPool = require('../eventPool');
+const { generateOrder, thankDriver } = require('./handlers');
+
+eventPool.on('DELIVERED', thankDriver);
+
+setInterval(() => {
+  console.log('-----------new interval begins-----------');
+  generateOrder();
+}, 5000);
