ae6c714f6eff1dfc1f96eef319240d06984fa853
Hunter Fehr <hunter.fehr3935@yahoo.com>
2022-12-14 19:39:39 -0800

    initial lab 13 commit

---
+++ driver/driver.test.js
@@ -0,0 +1,22 @@
+// 'use strict';
+
+// const { pickupInTransit, deliveryHandler } = require('./handlers');
+// const { io } = require('socket.io-client');
+// const socket = io('http://localhost:3001/caps');
+
+// jest.mock('socket', () => {
+//   return {
+//     on: jest.fn(),
+//     emit: jest.fn(),
+//   };
+// });
+// console.log = jest.fn();
+
+// describe('Driver', () => {
+//   const payload = {
+//     'store': '1-206-flowers',
+//     'orderId': 12345,
+//     'customer': 'Nemo',
+//     'address': '123 Wallaby Lane'
+//   };
+// });
